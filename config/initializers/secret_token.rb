5d3601add5b21b88d464a163551ad0f6e9cad6d2
Max <max.moreno@inzpiral.com>
2014-03-28 15:51:16 -0300

    inicializado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hola::Application.config.secret_key_base = '7fe393069d9dc342e6b01071968d61e5089d06ec31e1732ffc41d5aae3c3397600200583a735daef3833ac24486a39c5a43d81fbe9495ef88b4a1e29eacd9934'
